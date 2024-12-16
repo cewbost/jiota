bb479af553c95527860db9feb95a530805363634
Erik Boström <bostrom.ce@gmail.com>
2024-12-16 20:58:58 +0200

    add matchFields test case

--- test/match_fields.js
+++ test/match_fields.js
@@ -17,6 +17,12 @@ describe("matchFields", () => {
   it("should fail if matcher fails", () => {
     no_match(matchFields({"foo": 1, "bar": 2, "baz": 3}).match({"foo": 1, "bar": 1, "baz": 3}))
   })
+  it("should fail if nested matcher fails", () => {
+    no_match(matchFields({
+      "first": matchFields({"x": 1, "y": 2}),
+      "second": matchFields({"x": 3, "y": 4}),
+    }).match({"first": {"x": 1, "y": 2}, "second": {"x": 3, "y": 3}}))
+  })
   it("should fail if expected field does not exist", () => {
     no_match(matchFields({"foo": 1, "bar": 2, "baz": 3}).match({"foo": 1, "baz": 3}))
   })
