6f825757e973ecc634a1c717a7b92770f6c45bbb
Erik Boström <bostrom.ce@gmail.com>
2025-02-11 21:34:51 +0200

    move AssertionFailed to own file

---
+++ assertion_failed.js
@@ -0,0 +1,41 @@
+function indent(spaces, text) {
+  const ind = ("  ").repeat(spaces)
+  return text.replaceAll("\n", "\n" + ind)
+}
+
+function writeError(msg, level, obj) {
+  if (obj.object != null) {
+    msg += indent(level, "\nmatching: " + JSON.stringify(obj.object))
+    if (obj.sub != null) {
+      return writeError(msg, level, obj.sub)
+    }
+  }
+  const non_keys = ["errors", "object", "sub"]
+  let keys = Object.keys(obj)
+  for (const key of keys) {
+    if (non_keys.includes(key)) continue
+    msg += indent(level, `\n${key}:`)
+    msg = writeError(msg, level + 1, obj[key])
+  }
+  if (obj.errors != null) {
+    if (typeof obj.errors == "string") {
+      msg += indent(level, "\nerror: " + obj.errors)
+    } else {
+      msg += indent(level, "\nerrors:")
+      for (const err of obj.errors) msg += indent(level + 1, "\n- " + err)
+    }
+  }
+  return msg
+}
+
+class AssertionFailed extends Error {
+  constructor(obj, matcher, cause) {
+    const msg = "\nexpected " + JSON.stringify(obj) + "\nto " + matcher.description()
+    super(writeError(msg, 0, cause))
+    this.name = "AssertionFailed"
+  }
+}
+
+module.exports = {
+  AssertionFailed: AssertionFailed,
+}
