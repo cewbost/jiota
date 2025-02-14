ee8a989e2e2495c4adce42534075dcbc0165e098
Erik Boström <bostrom.ce@gmail.com>
2025-02-15 00:36:46 +0200

    add eventually to index

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-const { expect         } = require("./expect.js")
-const { equal          } = require("./equal.js")
-const { not            } = require("./not.js")
-const { matchFields    } = require("./match_fields.js")
-const { beNumber       } = require("./be_number.js")
-const { beInstanceOf   } = require("./be_instance_of.js")
-const { contain        } = require("./contain.js")
-const { consistOf      } = require("./consist_of.js")
-const { beEmpty        } = require("./be_empty.js")
-const { matchElements  } = require("./match_elements.js")
-const { beApprox       } = require("./be_approx.js")
-const { matchRegExp    } = require("./match_regexp.js")
-const { decoded        } = require("./decoded.js")
-const { capture        } = require("./capture.js")
+const { expect }         = require("./expect.js")
+const { eventually }     = require("./eventually.js")
+const { equal }          = require("./equal.js")
+const { not }            = require("./not.js")
+const { matchFields }    = require("./match_fields.js")
+const { beNumber }       = require("./be_number.js")
+const { beInstanceOf }   = require("./be_instance_of.js")
+const { contain }        = require("./contain.js")
+const { consistOf }      = require("./consist_of.js")
+const { beEmpty }        = require("./be_empty.js")
+const { matchElements }  = require("./match_elements.js")
+const { beApprox }       = require("./be_approx.js")
+const { matchRegExp }    = require("./match_regexp.js")
+const { decoded }        = require("./decoded.js")
+const { capture }        = require("./capture.js")
 const { throwException } = require("./throw.js")
 
 module.exports = {
   expect:         expect,
+  eventually:     eventually,
   equal:          equal,
   not:            not,
   matchFields:    matchFields,
